972f5c6c79b7e3e9edbb7ec9b13752837cddcc96
ParvathyRamakrishnan <153722269+ParvathyRamakrishnan@users.noreply.github.com>
2024-11-05 20:17:39 +0100

    Update quantized_tensor.py

--- quantized_tensor.py
+++ quantized_tensor.py
@@ -1,21 +1,3 @@
-"""
-Copyright 2020, ETH Zurich
-
-This file is part of RC-PyTorch.
-
-RC-PyTorch is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-RC-PyTorch is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with RC-PyTorch.  If not, see <https://www.gnu.org/licenses/>.
-"""
 import torch
 import types
 from torch import Tensor
